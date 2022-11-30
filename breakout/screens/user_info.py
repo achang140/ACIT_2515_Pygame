c7e4b77fc3e191c2e53d668d49927ba54e91e437
Amanda Chang <achang140@my.bcit.ca>
2022-11-29 16:31:07 -0800

    Modified user_info screen

--- breakout/screens/user_info.py
+++ breakout/screens/user_info.py
@@ -12,11 +12,15 @@ class UserInfo(BaseScreen):
         background = pygame.transform.scale(background_image, (800, 700))
         self.window.blit(background, (0, 0))
 
+        self.window.blit(self.ask_box.image, self.ask_box.rect)
         self.window.blit(self.name_box.image, self.name_box.rect)
 
     def update(self):
-        self.name_box = TextBox((300, 150), "Input Name: " + str(self.user_name), bgcolor = (255, 192, 77))
-        self.name_box.rect.topright = (550, 100)
+        self.ask_box = TextBox((100, 100), "Name: ", bgcolor = (255, 253, 246))
+        self.name_box = TextBox((200, 100), str(self.user_name), bgcolor = (255, 253, 150))        
+        self.ask_box.rect.topright = (460, 100)
+        self.name_box.rect.topright = (500, 200)
+    
 
     def manage_event(self, event):
         if event.type == pygame.KEYDOWN:
